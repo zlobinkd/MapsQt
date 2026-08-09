5cf6a301c04b33d7a2cefbb0d73d1fd19cf3ef5a
ABOBA <zlobinkd@gmail.com>
2026-08-09 13:34:13 +0200

    improve benchmark messages

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <QQmlApplicationEngine>
 #include <QApplication>
 #include "src/gui/mainWindow.h"
+#include "src/utils/benchmark.h"
+#include "src/map/map.h"
 
 static int browseMap(int argc, char *argv[]) {
     QApplication app(argc, argv);
@@ -14,7 +16,14 @@ static int browseMap(int argc, char *argv[]) {
     return app.exec();
 }
 
+static int benchmark() {
+	auto map = executeAndShowElapsedTime<Map>([]() { return Map{}; }, "Map creation");
+	executeAndShowElapsedTime([&map]() { map.shortestPath(359797, 645321); }, "Longest path calculation");
+	return 0;
+}
+
 int main(int argc, char *argv[])
 {
     browseMap(argc, argv);
+    //benchmark();
 }
--- src/utils/benchmark.h
+++ src/utils/benchmark.h
@@ -7,32 +7,22 @@
 
 // runs the function <func>, returns its output and prints the elapsed time to the command window
 // args: arguments for <func>
-template<class ReturnT, class F, class... Args> 
-ReturnT executeAndShowElapsedTime(const F& func, Args... args) {
+template<class ReturnT, class F, class... Args>
+ReturnT executeAndShowElapsedTime(const F& func, const std::string_view text, Args... args) {
 	auto begin = std::chrono::high_resolution_clock::now();
 	ReturnT res = func(args...);
 	auto end = std::chrono::high_resolution_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
-	qInfo() << duration.count() / 1000;
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+	qInfo() << text << ":" << duration.count() << "ms";
 	return res;
 }
 
 // runs the function <func> and prints the elapsed time to the command window
 template<class F>
-void executeAndShowElapsedTime(const F& func) {
-	auto begin = std::chrono::high_resolution_clock::now();
-	func();
-	auto end = std::chrono::high_resolution_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
-	qInfo() << duration.count() / 1000;
-}
-
-// runs the function <func> and prints the elapsed time to the command window
-template<class F>
-void executeAndShowElapsedTime(const F& func, const std::string& text) {
+void executeAndShowElapsedTime(const F& func, const std::string_view text) {
     auto begin = std::chrono::high_resolution_clock::now();
     func();
     auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
-    qInfo() << text << ": " << duration.count() / 1000;
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+    qInfo() << text << ":" << duration.count() << "ms";
 }
